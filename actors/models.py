4a7179b6e94aa6f38fdf35ed55d7ebf4a3bb017f
Jhonatan Keretz <jhonatan_keretz@hotmail.com>
2024-09-30 23:06:30 -0300

    first commit

---
+++ actors/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+NATIONALITY_CHOICES = (
+    ('USA', 'Estados Unidos'),
+    ('Brazil', 'Brasil'),
+    ('France', 'França'),
+    ('Germany', 'Alemanha'),
+    ('Argentina', 'Argentina'),
+)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=200)
+    birthday = models.DateField(null=True, blank=True)
+    nationality = models.CharField(
+        max_length=100,
+        choices=NATIONALITY_CHOICES,
+    )
+    
+    def __str__(self):
+        return self.name
